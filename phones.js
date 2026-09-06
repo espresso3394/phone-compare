ecfb3ba88d5e006515b05a2cd8a2d811e5c0618b
espresso3394 <espresso3394@gmail.com>
2026-09-06 17:24:08 +0900

    Create phones.js

---
+++ phones.js
@@ -0,0 +1,97 @@
+const phones = [
+    {
+        id: "iphone17",
+        name: "iPhone 17",
+        search: "iphone 17 아이폰17",
+        brand: "Apple",
+        year: 2025,
+        chipset: "Apple A19",
+        ram: "8GB",
+        display: "6.3인치 OLED",
+        refreshRate: "120Hz",
+        battery: "미표기",
+        camera: "48MP 메인",
+        weight: "177g",
+        os: "iOS"
+    },
+
+    {
+        id: "iphone17pro",
+        name: "iPhone 17 Pro",
+        search: "iphone 17 pro 아이폰17프로 17 pro",
+        brand: "Apple",
+        year: 2025,
+        chipset: "Apple A19 Pro",
+        ram: "12GB",
+        display: "6.3인치 OLED",
+        refreshRate: "120Hz",
+        battery: "미표기",
+        camera: "48MP 카메라 시스템",
+        weight: "206g",
+        os: "iOS"
+    },
+
+    {
+        id: "iphone17promax",
+        name: "iPhone 17 Pro Max",
+        search: "iphone 17 pro max 아이폰17프로맥스 17 pro max",
+        brand: "Apple",
+        year: 2025,
+        chipset: "Apple A19 Pro",
+        ram: "12GB",
+        display: "6.9인치 OLED",
+        refreshRate: "120Hz",
+        battery: "미표기",
+        camera: "48MP 카메라 시스템",
+        weight: "233g",
+        os: "iOS"
+    },
+
+    {
+        id: "galaxys21",
+        name: "Galaxy S21",
+        search: "galaxy s21 갤럭시s21 s21",
+        brand: "Samsung",
+        year: 2021,
+        chipset: "Exynos 2100 / Snapdragon 888",
+        ram: "8GB",
+        display: "6.2인치 Dynamic AMOLED 2X",
+        refreshRate: "120Hz",
+        battery: "4000mAh",
+        camera: "64MP 망원 + 12MP 광각 + 12MP 초광각",
+        weight: "169g",
+        os: "Android"
+    },
+
+    {
+        id: "galaxys21plus",
+        name: "Galaxy S21+",
+        search: "galaxy s21 plus 갤럭시s21플러스 s21+ s21 plus",
+        brand: "Samsung",
+        year: 2021,
+        chipset: "Exynos 2100 / Snapdragon 888",
+        ram: "8GB",
+        display: "6.7인치 Dynamic AMOLED 2X",
+        refreshRate: "120Hz",
+        battery: "4800mAh",
+        camera: "64MP 망원 + 12MP 광각 + 12MP 초광각",
+        weight: "200g",
+        os: "Android"
+    },
+
+    {
+        id: "galaxys21ultra",
+        name: "Galaxy S21 Ultra",
+        search: "galaxy s21 ultra 갤럭시s21울트라 s21 ultra 울트라",
+        brand: "Samsung",
+        year: 2021,
+        chipset: "Exynos 2100 / Snapdragon 888",
+        ram: "12GB / 16GB",
+        display: "6.8인치 Dynamic AMOLED 2X",
+        refreshRate: "120Hz",
+        battery: "5000mAh",
+        camera: "108MP 메인",
+        weight: "약 227g",
+        os: "Android"
+    }
+];
